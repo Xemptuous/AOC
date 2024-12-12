963c85ac0d309eb7775749157c931bdec738f5b6
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2024-12-11 20:54:02 -0800

    d11p1 optimized

--- 2024/Rust/d11p1.rs
+++ 2024/Rust/d11p1.rs
@@ -1,42 +1,46 @@
+use std::collections::LinkedList;
+
 fn main() {
     let string = std::fs::read_to_string("inputs/d11.in").expect("Couldn't read file");
-    let mut numbers: Vec<String> = string.split_whitespace().map(String::from).collect();
-    numbers.reserve(1000000);
+    let numbers: Vec<String> = string.split_whitespace().map(String::from).collect();
+
+    let mut head: LinkedList<String> = LinkedList::new();
+    for number in numbers {
+        head.push_back(number);
+    }
 
     for _ in 0..25 {
-        // 25 blinks
-        let mut i = 0;
-        while i < numbers.len() {
-            let n = numbers[i].clone();
+        let iter = head.iter();
+        let mut dummy: LinkedList<String> = LinkedList::new();
+        for s in iter {
             // Rule 1: if 0, then 1
-            if n == "0" {
-                numbers[i].remove(0);
-                numbers[i].insert(0, '1');
-                i += 1;
+            if s == "0" {
+                dummy.push_back(String::from("1"));
             }
             // Rule 2: if even number, split into 2. No leading zeros (for right)
-            else if n.len() % 2 == 0 {
-                let (left, right) = n.split_at(n.len() / 2);
-                // replace left number
-                numbers[i] = String::from(left);
-                if right.starts_with('0') {
-                    if !right.chars().all(|c| c == '0') {
-                        numbers.insert(i + 1, String::from(right.trim_start_matches('0')));
-                    } else {
-                        numbers.insert(i + 1, String::from("0"));
-                    }
-                } else {
-                    numbers.insert(i + 1, String::from(right));
-                }
-                i += 2;
+            else if s.len() % 2 == 0 {
+                let (left, right) = s.split_at(s.len() / 2);
+                dummy.push_back(left.to_string());
+                dummy.push_back(parse_right(right));
             }
             // Rule 3: else, * 2024
             else {
-                let num = n.parse::<u64>().unwrap();
-                numbers[i] = (num * 2024).to_string();
-                i += 1;
+                let num = (s.parse::<u64>().unwrap() * 2024).to_string();
+                dummy.push_back(num);
             }
         }
+        head = dummy;
     }
-    println!("{}", numbers.len());
+    println!("{}", head.len());
+}
+
+fn parse_right(right: &str) -> String {
+    if right.starts_with('0') {
+        if !right.chars().all(|c| c == '0') {
+            return String::from(right.trim_start_matches('0'));
+        } else {
+            return String::from("0");
+        }
+    }
+    String::from(right)
 }
